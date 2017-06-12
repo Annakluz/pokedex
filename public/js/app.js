659b60378ab5abdcae6685b684f7d1fadcde2bed
Anna karina Luz <anna.luz.duran@gmail.com>
2017-06-12 14:28:33 -0500

    nombres

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,34 @@
-$.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
-    var pokemones = response.results;
-    agregarPokemones(pokemones);
-});
-
-function agregarPokemones(pokemones){
-    pokemones.forEach(function(pokemon){
-        $("#lista_pokemones").append($("<li />").text(pokemon.name));
-        
-    });
-    
+var cargarPagina = function(){
+  cargarPokemon();
+ // $(document).on("click", ".pokemon", mostrarDetallePokemon);
 };
+
+var cargarPokemon = function (){
+    var url = "http://pokeapi.co/api/v2/pokemon/"
+     $.getJSON(url, function (response) {
+        // console.log(response); con esto me da la cantidad de objetos.
+        var pokemones = response.results;
+        mostrarPokemones(pokemones);
+  
+
+    });
+
+};
+
+var mostrarPokemones = function (pokemones){
+    var $ul= $("#lista_pokemones");
+    pokemones.forEach(function(pokemon){
+       //console.log (pokemon.name);
+        var $li = $("<li />");
+        $li.text(pokemon.name);
+        $ul.append($li);
+       
+        //console.log($li);
+    });
+}
+
+
+
+
+
+$(document).ready(cargarPagina);
